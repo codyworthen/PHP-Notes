472c29037d3bfb4bc1c738d812829a95362a0327
Cody Worthen <cworthen777@hotmail.com>
2022-06-12 14:26:01 -0700

    require keyword

---
+++ index.view.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html lang="eng">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>Document</title>
+
+    <style>
+
+        header {
+            background-color: gray;
+            padding: 2em;
+            text-align: center;
+        }
+
+        body {
+            text-align: center;
+        }
+
+    </style>
+
+</head>
+
+<body>
+
+<header>
+    <h1>
+        <?= $greeting; ?>
+    </h1>
+</header>
+
+</body>
+
+</html>
+
