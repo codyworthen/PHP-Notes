9f7bb8e2480d8b1bcc2be4867b7920c36dca8619
Cody Worthen <cody@cerebrumcorp.com>
2022-07-05 14:11:53 -0700

    EP 20 done

--- src/forms and request types/core/Request.php
+++ src/forms and request types/core/Request.php
@@ -1,16 +1,13 @@
 <?php
 
-class Request
-{
-    public static function uri()
-    {
-        return trim(
-            parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'
-        );
-    }
-
-    public static function method()
-    {
-        return $_SERVER['REQUEST_METHOD'];
-    }
+class Request {
+	public static function uri() {
+		return trim(
+			parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'
+		);
+	}
+	
+	public static function method() {
+		return $_SERVER['REQUEST_METHOD'];
+	}
 }
